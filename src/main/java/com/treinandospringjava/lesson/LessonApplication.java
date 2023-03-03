65c3aa042deecd3e1964ea3c5d7f289d8f121acc
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-03-03 17:33:46 -0300

    Project started!

---
+++ src/main/java/com/treinandospringjava/lesson/LessonApplication.java
@@ -0,0 +1,14 @@
+package com.treinandospringjava.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LessonApplication.class, args);
+	}
+
+}
